8761fabf35de17d1c7c5777ecc23dbe4aee233cf
heidiali <heidi.alikippari@gmail.com>
2019-03-21 15:00:39 +0200

    added NameSearch model

---
+++ Loppuprojekti_MVC/Models/NameSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loppuprojekti_MVC.Models
+{
+    public class NameSearch
+    {
+        public int Taxonid { get; set; }
+        public string MainCommonName { get; set; }
+        public string ScientificName { get; set; }
+        public string Category { get; set; }
+        public string PopulationTrend { get; set; }
+
+    }
+}
