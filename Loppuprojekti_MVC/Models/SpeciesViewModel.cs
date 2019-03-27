28aea3d98e73b065d56747f32602c9d41055c3c4
heidiali <heidi.alikippari@gmail.com>
2019-03-27 07:23:00 +0200

    button attempt

---
+++ Loppuprojekti_MVC/Models/SpeciesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loppuprojekti_MVC.Models
+{
+    public class SpeciesViewModel
+    {
+        public Species Species { get; set; }
+        public SpeciesCategory SpeciesCategory { get; set; }
+    }
+}
