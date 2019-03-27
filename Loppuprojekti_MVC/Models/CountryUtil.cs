5a675677a3ad242b6cee06458a0ec1bc2d216cf4
anpumi <annukka.puotiniemi@gmail.com>
2019-03-27 11:49:05 +0200

    Changes to get country species details from a local text file

--- Loppuprojekti_MVC/Models/CountryUtil.cs
+++ Loppuprojekti_MVC/Models/CountryUtil.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -35,24 +36,43 @@ namespace Loppuprojekti_MVC.Models
 
             }
         }
+        // edit needed below to get data from local storage 
+        //public List<CountrySpecies> Country(string isoCode) // parameter needed for the var response url below
+        //{
+        //    string json = "";
+
+        //    using (var client = new HttpClient())
+        //    {
+        //        CountryList cl = new CountryList();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        //        var response = client.GetAsync($"http://apiv3.iucnredlist.org/api/v3/country/getspecies/{isoCode}?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee").Result;
+        //        //var response = client.GetAsync($"http://apiv3.iucnredlist.org/api/v3/country/getspecies/AZ?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee").Result;
+        //        var responseString = response.Content.ReadAsStringAsync().Result;
+        //        json = responseString;
+        //        {
+        //            CountriesRoot ct;
+        //            ct = JsonConvert.DeserializeObject<CountriesRoot>(json);
+        //            return ct.Result;
+        //        }
+        //    }
+        //}
+
         public List<CountrySpecies> Country(string isoCode) // parameter needed for the var response url below
         {
             string json = "";
 
-            using (var client = new HttpClient())
+            using (StreamReader sr = new StreamReader(@"C:\Users\Annukka\academy\Loppuprojekti\CountryAPi\" + isoCode + ".txt"))
             {
                 CountryList cl = new CountryList();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var response = client.GetAsync($"http://apiv3.iucnredlist.org/api/v3/country/getspecies/{isoCode}?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee").Result;
-                //var response = client.GetAsync($"http://apiv3.iucnredlist.org/api/v3/country/getspecies/AZ?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee").Result;
-                var responseString = response.Content.ReadAsStringAsync().Result;
-                json = responseString;
-                {
+                json = sr.ReadToEnd();
                     CountriesRoot ct;
                     ct = JsonConvert.DeserializeObject<CountriesRoot>(json);
                     return ct.Result;
-                }
+                    //return ct.Result;
+
             }
+
+            
         }
     }
 }
